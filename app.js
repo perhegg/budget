e0e6bbc2cc50d4291511f47c5562e654d74de2a2
perhegg <per_hegg@hotmail.com>
2019-10-18 13:47:43 +0200

    adding new item to budget controller

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // BUDGET CONTROLLER
-var budgetController = (function(){
+var budgetController = (function() {
 
     var Expense = function(id, description, value) {
         this.id = id;
@@ -13,15 +13,44 @@ var budgetController = (function(){
         this.value = value;
     };
 
-
     var data = {
-        allitems: {
+        allItems: {
             exp: [],
             inc: []
         },
         totals: {
             exp: 0,
             inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
         }
     }
 
@@ -41,9 +70,9 @@ var UIController = (function(){
     return {
         getInput: function() {
             return {
-                type: document.querySelector(DOMstrings.inputType).value,
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -72,11 +101,11 @@ var controller = (function(budgetCtrl, UICtrl){
 
 
     var ctrlAddItem = function(){
-
+        var input, newItem;
         // Add field input data
             input = UICtrl.getInput();
         // Add the item to the budget controller
-
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // Add the item to the ui
 
         // Calculate the budget
