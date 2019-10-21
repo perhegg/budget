029d87c62e53d5261ad41992b43185c80b53d65c
perhegg <per_hegg@hotmail.com>
2019-10-21 17:55:55 +0200

    Setup delete listeners

--- app.js
+++ app.js
@@ -103,7 +103,8 @@ var UIController = (function(){
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
     };
 
     return {
@@ -184,6 +185,8 @@ var controller = (function(budgetCtrl, UICtrl){
                 ctrlAddItem();
             }
         });
+
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     };
 
     var updateBudget = function(){
@@ -210,6 +213,28 @@ var controller = (function(budgetCtrl, UICtrl){
         // Calculate and update budget
         updateBudget();
         }
+    }
+
+    var ctrlDeleteItem = function(event) {
+        var itemID, splitID, type, ID;
+
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if (itemID) {
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = splitID[1];
+
+            // 1. Delete the item from the data structure
+
+            // 2. Delete the item from the UI
+
+            // 3. Update and show the new budget
+        }
+
+
+        console.log(itemID)
+
     }
 
     return {
