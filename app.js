fbdd65f70d8bb8016810fbeba28814e3b6374294
perhegg <per_hegg@hotmail.com>
2019-10-21 14:39:32 +0200

    Clear the input fields

--- app.js
+++ app.js
@@ -101,6 +101,19 @@ var UIController = (function(){
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
         },
 
+        clearFields: function(){
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -133,6 +146,9 @@ var controller = (function(budgetCtrl, UICtrl){
             newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // Add the item to the ui
             UICtrl.addListItem(newItem, input.type);
+
+        // Clear the fields
+            UICtrl.clearFields();
         // Calculate the budget
 
         // Display the budget
