bb5c437ded2b4b653e8ad595a19d955d7d9b952c
perhegg <per_hegg@hotmail.com>
2019-10-21 17:14:48 +0200

    Updating the UI controller

--- app.js
+++ app.js
@@ -99,7 +99,11 @@ var UIController = (function(){
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -147,6 +151,18 @@ var UIController = (function(){
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+            if(obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -176,7 +192,7 @@ var controller = (function(budgetCtrl, UICtrl){
         // Return budget
         var budget = budgetCtrl.getBudget();
         // Display the budget on the display
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     }
 
     var ctrlAddItem = function(){
@@ -199,6 +215,12 @@ var controller = (function(budgetCtrl, UICtrl){
     return {
         init: function(){
             console.log('app has started')
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners()
         }
     }
